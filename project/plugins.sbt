54068fb76a96fb3bf37e0687f99b586ad5efe856
Lloyd <lloydmeta@users.noreply.github.com>
2017-06-24 02:47:48 +0900

    Update to Play 2.6.0 (closes #2) (#3)
    Fairly straight-forward upgrade. Asides from reformatting and dependencies, this resulted in a net 0 change of actual code (2 lines added, 2 lines deleted).

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,6 @@
 // The Play plugin
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.10")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.0")
 
-addSbtPlugin("com.geirsson" % "sbt-scalafmt" % "0.4.10")
+addSbtPlugin("com.lucidchart" % "sbt-scalafmt" % "1.6")
+
+addSbtPlugin("com.lucidchart" % "sbt-scalafmt-coursier" % "1.6")
--- test/MyCompileTimeSpecs.scala
+++ test/MyCompileTimeSpecs.scala
@@ -1,34 +1,35 @@
-import helpers.{OneAppPerSuiteWithComponents, OneAppPerTestWithComponents, OneServerPerSuiteWithComponents, OneServerPerTestWithComponents}
+import helpers.{
+  OneAppPerSuiteWithComponents,
+  OneAppPerTestWithComponents,
+  OneServerPerSuiteWithComponents,
+  OneServerPerTestWithComponents
+}
 import org.scalatest.TestSuite
 import play.api.ApplicationLoader.Context
 import play.api.libs.ws.ahc.AhcWSComponents
 
 class AppWithTestComponents(context: Context) extends AppComponents(context) with AhcWSComponents
 
-trait OneAppPerTestWithMyComponents
-  extends OneAppPerTestWithComponents[AppWithTestComponents] {
+trait OneAppPerTestWithMyComponents extends OneAppPerTestWithComponents[AppWithTestComponents] {
   this: TestSuite =>
 
   override def createComponents(context: Context) = new AppWithTestComponents(context)
 }
 
-trait OneAppPerSuiteWithMyComponents
-  extends OneAppPerSuiteWithComponents[AppWithTestComponents] {
+trait OneAppPerSuiteWithMyComponents extends OneAppPerSuiteWithComponents[AppWithTestComponents] {
   this: TestSuite =>
 
   override def createComponents(context: Context) = new AppWithTestComponents(context)
 }
 
 trait OneServerPerTestWithMyComponents
-  extends OneServerPerTestWithComponents[AppWithTestComponents] {
-  this: TestSuite =>
+    extends OneServerPerTestWithComponents[AppWithTestComponents] { this: TestSuite =>
 
   override def createComponents(context: Context) = new AppWithTestComponents(context)
 }
 
 trait OneServerPerSuiteWithMyComponents
-  extends OneServerPerSuiteWithComponents[AppWithTestComponents] {
-  this: TestSuite =>
+    extends OneServerPerSuiteWithComponents[AppWithTestComponents] { this: TestSuite =>
 
   override def createComponents(context: Context) = new AppWithTestComponents(context)
 }
